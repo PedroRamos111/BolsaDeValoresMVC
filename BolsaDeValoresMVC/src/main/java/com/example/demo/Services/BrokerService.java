428f3735f00e9488da498458f745d44ae8464009
GiovanniBSDuarte <giovannibsduarte@gmail.com>
2024-06-26 11:16:32 -0300

    asd

--- BolsaDeValoresMVC/src/main/java/com/example/demo/Services/BrokerService.java
+++ BolsaDeValoresMVC/src/main/java/com/example/demo/Services/BrokerService.java
@@ -1,27 +1,70 @@
 package com.example.demo.Services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import com.example.demo.Models.Broker;
+import com.example.demo.Models.Livro;
+import com.example.demo.Models.Transacao;
 import com.example.demo.Repositories.BrokerRepository;
+import com.example.demo.Repositories.LivroRepository;
+import com.example.demo.Repositories.TransacaoRepository;
 
 @Service
-public class BrokerService implements MessageListener  {
+public class BrokerService implements MessageListener {
 
     private String msgFormatada;
-    
+
     public String getFormattedMessage() {
         return this.msgFormatada;
     }
+
     @Autowired
     private BrokerRepository brokerRepository;
     @Autowired
+    private LivroRepository livroRepository;
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+    @Autowired
     private RabbitMQTopicInitializer rabbitMQTopicInitializer;
+
+    private List<Livro> todasCompras = new ArrayList<>();
+    private List<Livro> todasVendas = new ArrayList<>();
+    private List<Transacao> transacoes = new ArrayList<>();
+
+    
+    public List<Livro> getTodasCompras() {
+        return todasCompras;
+    }
+
+    public List<Livro> getTodasVendas() {
+        return todasVendas;
+    }
+
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }
+
+
+    @PostConstruct
+    public void init() {
+        todasCompras = livroRepository.findByAtividade("compra");
+        todasVendas = livroRepository.findByAtividade("venda");
+        transacoes = transacaoRepository.findAll();
+    }
+
+
 
     public Broker findByName(String name) {
         return brokerRepository.findByName(name);
@@ -50,15 +93,27 @@ public class BrokerService implements MessageListener  {
     private RabbitTemplate rabbitTemplate;
 
     public void compra(String ativo, int quant, double val, String corretora) {
-        String topic = "compra." + ativo;
-        String message = quant + ";" + val + ";" + corretora;
-        enviaPedido(topic, message);
+        Livro livro = new Livro();
+        livro.setAtividade("compra");
+        livro.setComprador(corretora);
+        livro.setBolsa(ativo);
+        livro.setQtd(quant);
+        livro.setValor(val);
+        livroRepository.save(livro);
+        todasCompras.add(livro);
+        enviaPedido("compra." + ativo, quant + ";" + val + ";" + corretora);
     }
 
     public void venda(String ativo, int quant, double val, String corretora) {
-        String topic = "venda." + ativo;
-        String message = quant + ";" + val + ";" + corretora;
-        enviaPedido(topic, message);
+        Livro livro = new Livro();
+        livro.setAtividade("venda");
+        livro.setComprador(corretora);
+        livro.setBolsa(ativo);
+        livro.setQtd(quant);
+        livro.setValor(val);
+        livroRepository.save(livro);
+        todasVendas.add(livro);
+        enviaPedido("venda." + ativo, quant + ";" + val + ";" + corretora);
     }
 
     private void enviaPedido(String topic, String message) {
@@ -67,8 +122,8 @@ public class BrokerService implements MessageListener  {
         System.out.println(" [x] Sent '" + message + "'");
     }
 
-
-    @RabbitListener(queues = "BOLSA") // isso esta errado de proposito pq tava quebrando c os split q n existia
+ 
+    @RabbitListener(queues = "BOLSA")
     public void recebeMsg(String message) {
         String[] dadosM = message.split(";");
         String tipo = dadosM[0];
@@ -76,6 +131,13 @@ public class BrokerService implements MessageListener  {
         String quantidade = dadosM[2];
         String preco = dadosM[3];
         String corretora = dadosM[4];
+        Transacao transacao = new Transacao();
+        transacao.setComprador(corretora);
+        transacao.setBolsa(acao);
+        transacao.setQtd(Integer.parseInt(quantidade));
+        transacao.setValor(Double.parseDouble(preco));
+        transacaoRepository.save(transacao);
+        transacoes.add(transacao);
         System.out.println(formatMsg(tipo, acao, quantidade, preco, corretora));
     }
 
@@ -117,7 +179,7 @@ public class BrokerService implements MessageListener  {
 
     @Override
     public void onMessage(Message msg) {
-        String msgS = new String(msg.getBody()); 
+        String msgS = new String(msg.getBody());
         String[] dadosM = msgS.split(";");
         String tipo = dadosM[0];
         String acao = dadosM[1];
